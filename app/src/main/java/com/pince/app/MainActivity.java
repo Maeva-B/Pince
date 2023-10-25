2a9979658182abc9a67fe98b93af73f995839b5d
Maëva B <maeva.burillo@u-psud.fr>
2023-10-25 17:47:05 +0200

    correction onglet + alignement carte

--- app/src/main/java/com/pince/app/MainActivity.java
+++ app/src/main/java/com/pince/app/MainActivity.java
@@ -3,7 +3,12 @@ package com.pince.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayout;
+import androidx.cardview.widget.CardView;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -11,18 +16,14 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    */
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Date et heure ---------------
+
 
         // Récupérer le TextView pour la date et l'heure
         TextView dateTimeTextView = findViewById(R.id.textViewDateTime);
@@ -34,5 +35,62 @@ public class MainActivity extends AppCompatActivity {
 
         // Définir la date et l'heure dans le TextView
         dateTimeTextView.setText(formattedDate);
+
+
+        // --------------------------------
+
+
+
+        // Onglets ---------------
+
+        // Récupérez une référence à votre TabLayout
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+
+        // Sélectionnez l'onglet "Preset" par défaut
+        TabLayout.Tab tab = tabLayout.getTabAt(0); // 0 pour le premier onglet, 1 pour le deuxième, etc.
+        if (tab != null) {
+            tab.select();
+        }
+
+        // Définissez un écouteur de sélection d'onglet
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // L'onglet "Preset" est sélectionné
+                if (tab.getPosition() == 0) {
+                    Button presetButton1 = findViewById(R.id.presetButton1);
+                    Button presetButton2 = findViewById(R.id.presetButton2);
+                    Button presetButton3 = findViewById(R.id.presetButton3);
+                    Button presetButton4 = findViewById(R.id.presetButton4);
+                    presetButton1.setVisibility(View.VISIBLE);
+                    presetButton2.setVisibility(View.VISIBLE);
+                    presetButton3.setVisibility(View.GONE);
+                    presetButton4.setVisibility(View.GONE);
+                }
+                // L'onglet "Personnalisable" est sélectionné
+                else if (tab.getPosition() == 1) {
+                    Button presetButton1 = findViewById(R.id.presetButton1);
+                    Button presetButton2 = findViewById(R.id.presetButton2);
+                    Button presetButton3 = findViewById(R.id.presetButton3);
+                    Button presetButton4 = findViewById(R.id.presetButton4);
+                    presetButton1.setVisibility(View.GONE);
+                    presetButton2.setVisibility(View.GONE);
+                    presetButton3.setVisibility(View.VISIBLE);
+                    presetButton4.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // Ne rien faire ici
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // Ne rien faire ici
+            }
+        });
+
+        // -----------------------------------------
     }
 }
